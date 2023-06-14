1fdc654b571bf8af098bafff54c0525901c9b063
Gideon Kimutai <gideon.kimutai.kim@gmail.com>
2023-06-14 06:26:33 +0300

    chore: update linting tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,28 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'eslint:recommended',
-    'standard-with-typescript',
-    'plugin:angular/johnpapa'
+  root: true,
+  overrides: [
+    {
+      files: ["*.ts"],
+      extends: ["eslint:recommended", "plugin:prettier/recommended"],
+    },
+    // NOTE: WE ARE NOT APPLYING PRETTIER IN THIS OVERRIDE, ONLY @ANGULAR-ESLINT/TEMPLATE
+    {
+      files: ["*.html"],
+      extends: ["plugin:@angular-eslint/template/recommended"],
+      rules: {},
+    },
+    // NOTE: WE ARE NOT APPLYING @ANGULAR-ESLINT/TEMPLATE IN THIS OVERRIDE, ONLY PRETTIER
+    {
+      files: ["*.html"],
+      excludedFiles: ["*inline-template-*.component.html"],
+      extends: ["plugin:prettier/recommended"],
+      rules: {
+        // NOTE: WE ARE OVERRIDING THE DEFAULT CONFIG TO ALWAYS SET THE PARSER TO ANGULAR (SEE BELOW)
+        "prettier/prettier": ["error", { parser: "angular" }],
+      },
+    },
   ],
-  plugins: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json']
-  },
   rules: {
-    '@typescript-eslint/consistent-type-imports': 'off'
-  }
-}
+    "@typescript-eslint/consistent-type-imports": "off",
+  },
+};
