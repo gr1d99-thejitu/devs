65fcca7d97854cf34a8689cc64336a4dfa2091e4
Gideon Kipkogei Kimutai <gideon.kimutai@thejitu.com>
2023-06-23 15:48:27 +0300

    refactor: update developer form validations on demand

--- src/app/modules/developers/new-developer/new-developer.component.ts
+++ src/app/modules/developers/new-developer/new-developer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ProgrammingLanguageService } from '../../../services/programming-language.service';
 
@@ -7,16 +7,20 @@ import { ProgrammingLanguageService } from '../../../services/programming-langua
   templateUrl: './new-developer.component.html',
   styleUrls: ['./new-developer.component.scss'],
 })
-export class NewDeveloperComponent implements OnInit {
+export class NewDeveloperComponent implements OnInit, OnChanges {
+  canCreateNewUser = false;
+
   form = this.formBuilder.group({
     createUser: [false],
     user: this.formBuilder.group({
+      existingUser: ['', Validators.required],
       fullNames: ['', Validators.required],
       email: ['', Validators.required, Validators.email],
     }),
     developer: this.formBuilder.group({
       title: ['', Validators.required],
       skills: [''],
+      activateAccount: [false],
     }),
   });
 
@@ -25,7 +29,33 @@ export class NewDeveloperComponent implements OnInit {
     private programmingLanguagesService: ProgrammingLanguageService
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.form.controls.createUser.valueChanges.subscribe((value) => {
+      if (value !== null) {
+        this.canCreateNewUser = value;
+
+        this.updateValidators();
+      }
+    });
+  }
+
+  updateValidators() {
+    if (this.canCreateNewUser) {
+      this.form.controls.user.controls.existingUser.removeValidators(
+        Validators.required
+      );
+    } else {
+      this.form.controls.user.controls.fullNames.removeValidators(
+        Validators.required
+      );
+      this.form.controls.user.controls.email.removeValidators([
+        Validators.required,
+        Validators.email,
+      ]);
+    }
+  }
+
+  ngOnChanges() {}
 
   onSubmit() {
     console.log(this.form.value);
