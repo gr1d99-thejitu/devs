49ce0ac1f7eda0c79e9a014144548db907a475e0
Gideon Kimutai <gideon.kimutai.kim@gmail.com>
2023-06-25 11:05:35 +0300

    chore: guard developers route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,11 @@ const routes: Routes = [
   },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'developers/new', component: NewDeveloperComponent },
+  {
+    path: 'developers/new',
+    component: NewDeveloperComponent,
+    canActivate: [authGuard],
+  },
 ];
 
 @NgModule({
